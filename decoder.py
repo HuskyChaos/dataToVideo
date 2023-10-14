1ecd32a1c16ca11d57b74c4b29ed81462ebe7ba0
Vikash Kumar <vikashkrdgr@gmail.com>
2023-10-14 04:52:15 -0400

    decoder ready

--- decoder.py
+++ decoder.py
@@ -1,31 +1,87 @@
 #!/usr/bin/python3
 from PIL import Image
-import cv2,os
+import os, time, subprocess, base64
 
-vid = cv2.VideoCapture("./video.avi")
-os.system('mkdir pictures')
+# Installing opencv
+try:
+    import cv2
+except ImportError or ModuleNotFoundError:
+    print('\033[31m[!] CV2 not installed\033[00m')
+    print('[+] Installing CV2')
+    cv2 = subprocess.check_output(['pip3', 'install', 'opencv-python'], text=True)
+    if "Successfully installed" in cv2:
+        print('[+] Successfully installed CV2')
+        time.sleep(2)
+        import cv2
+    else:
+        print('\033[31m[!] error\033[00m')
+        quit()
+
+# Searching for file
+fileName = input('\033[32m[-] Enter file name: ')
+if fileName[-3:] != 'avi':
+    print('\033[31m[!] Invalid file extenssion!\033[00m')
+    quit()
+whoAmI = '/home/'
+whoAmI+= subprocess.check_output(['whoami'], text=True)
+find_file = subprocess.check_output(['find', whoAmI.strip(), '-type', 'f', '-name', fileName], text=True)
+fileList = find_file.strip().split('\n')
+if len(find_file) == 0:
+    print('\033[31m[!] File not found\033[00m')
+    quit()
+else:
+    if len(fileList) > 1:
+        num = 0
+        for file in fileList:
+            num+=1
+            print(f'[{num}] : {file}')
+        selectedFile = int(input('\n[-] Select File Index Number : '))
+        fileName = fileList[selectedFile-1]
+    else:
+        fileName = fileList[0]
+
+vid = cv2.VideoCapture(f"{fileName.strip()}")
+print('[+] Extracting Frames')
+os.system('mkdir outPictures')
 currentframe = 1
 while(True):
     ret,frame = vid.read()
     if ret:
-        name = './pictures/frame' + str(currentframe) + '.png'
+        name = './outPictures/frame' + str(currentframe) + '.png'
         cv2.imwrite(name, frame)
         currentframe += 1
     else:
         break
 vid.release()
 cv2.destroyAllWindows()
-exit()
-im = Image.open('x.bmp')
-
-imageSizeW, imageSizeH = im.size
-
-nonWhitePixels = []
-
-for i in range(1, imageSizeW):
-    for j in range(1, imageSizeH):
-        pixVal = im.getpixel((i, j))
-        if pixVal != (255, 255, 255):
-            nonWhitePixels.append([i, j])
-
-print(nonWhitePixels)
+b64Data = ''
+imageList = os.listdir('./outPictures/')
+bits = ''
+print('[+] Decoding file')
+for image in imageList:
+    im = Image.open(f'outPictures/{image}')
+    imageSizeW, imageSizeH = im.size
+    pixel_list = list(im.getdata())
+    for i in range(0, imageSizeH):
+        for j in range(0, imageSizeW):
+            pixVal = im.getpixel((j, i))
+            if pixVal[0] == 0 and pixVal[1] == 0 and pixVal[2] == 0:
+                if len(bits) == 7:
+                    b64Data+=chr(int(bits, 2))
+                    bits=''
+                    bits+='1'
+                else:
+                    bits+='1'
+            else:
+                if len(bits) == 7:
+                    b64Data+=chr(int(bits, 2))
+                    bits=''
+                    bits+='0'
+                else:
+                    bits+='0'
+print('[+] Creating file')
+createFile = open(f'decodedFile.{fileName.split("-")[-1].split(".")[-2]}', 'wb')
+createFile.write(base64.b64decode(b64Data))
+print('[+] CleaningUp!\033[00m')
+os.system(f'rm -rf {fileName}')
+os.system('rm -rf outPictures')
--- encoder.py
+++ encoder.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 from PIL import Image
-import os, subprocess, base64
+import os, subprocess, base64, time
 import numpy as np
+import magic
 
 # Installing opencv
 try:
@@ -12,6 +13,7 @@ except ImportError or ModuleNotFoundError:
     cv2 = subprocess.check_output(['pip3', 'install', 'opencv-python'], text=True)
     if "Successfully installed" in cv2:
         print('[+] Successfully installed CV2')
+        time.sleep(2)
         import cv2
     else:
         print('\033[31m[!] error\033[00m')
@@ -23,20 +25,23 @@ count1080 = 0
 count720 = 0
 picnumber = 0
 bitCounter = 0
+pixelX = 1080
+pixelY = 720
 
 def __createPicture__(j):
+    # catch the set here
     global count1080,count720,pixel,pixelSet,picnumber
     count1080+=1
     if j == '0':
         pixel.append((225, 225, 225))
     elif j == '1':
         pixel.append((0, 0, 0))
-    if count1080 == 1080:
+    if count1080 == pixelX:
         pixelSet.append(pixel)
         pixel=[]
         count720+=1
         count1080=0
-    if count720 == 720:
+    if count720 == pixelY:
         picnumber+=1
         array = np.array(pixelSet, dtype=np.uint8)
         new_image = Image.fromarray(array)
@@ -45,7 +50,7 @@ def __createPicture__(j):
         count720 = 0
 
 # Searching for file
-fileName = input('\033[32mEnter file name: ')
+fileName = input('\033[32m[-] Enter file name: ')
 whoAmI = '/home/'
 whoAmI+= subprocess.check_output(['whoami'], text=True)
 find_file = subprocess.check_output(['find', whoAmI.strip(), '-type', 'f', '-name', fileName], text=True)
@@ -59,10 +64,12 @@ else:
         for file in fileList:
             num+=1
             print(f'[{num}] : {file}')
-        selectedFile = int(input('\nSelect File Index Number : '))
+        selectedFile = int(input('\n[-] Select File Index Number : '))
         fileName = fileList[selectedFile-1]
     else:
         fileName = fileList[0]
+
+fileType = magic.from_file(fileName).split()[0]
 
 targetFile = open(fileName.strip(), 'rb')
 targetFile = targetFile.read()
@@ -70,10 +77,10 @@ targetFile = targetFile.read()
 print('[+] Encoding file')
 b64File = base64.b64encode(targetFile)
 fileBits = len((b64File)*7)
-count = int(fileBits/(1080*720))
-extraBits = fileBits - count * (1080*720)
+count = int(fileBits/(pixelX*pixelY))
+extraBits = fileBits - count * (pixelX*pixelY)
 
-print('[+] Creating pictures')
+print('[+] Creating Frames')
 subprocess.run(['mkdir', 'pictures'])
 remainingBits = []
 for i in b64File:
@@ -87,15 +94,18 @@ for i in b64File:
         else:
             remainingBits.append(j)
 
-for i in range(0, (1080*720)-extraBits):
+for i in range(0, (pixelX*pixelY)-extraBits):
     remainingBits.append('1')
 
 for j in remainingBits:
     __createPicture__(j)
 
 image_folder = 'pictures'
-video_name = 'video.avi'
-images = [img for img in os.listdir(image_folder) if img.endswith(".png")]
+video_name = input('[-] Enter desired video name: ')
+video_name = f'{video_name}-{fileType.lower()}.avi'
+print('[+] Do not remove the file type from the file name')
+images = os.listdir(image_folder)
+images.reverse()
 frame = cv2.imread(os.path.join(image_folder, images[0]))
 height, width, layers = frame.shape
 video = cv2.VideoWriter(video_name, 0, 1, (width,height))
